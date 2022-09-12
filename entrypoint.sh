c09e595b9217a08d2d846d5968975f8a442f8b06
Óscar de la Mata <oscar@hanzo.es>
2022-09-12 21:39:02 +0200

    upd: force on git push

--- entrypoint.sh
+++ entrypoint.sh
@@ -65,7 +65,7 @@ if git status | grep -q "Changes to be committed"
 then
   git commit --message "$INPUT_COMMIT_MESSAGE"
   echo "Pushing git commit"
-  git push -u origin HEAD:"$OUTPUT_BRANCH"
+  git push -u -f origin HEAD:"$OUTPUT_BRANCH"
 else
   echo "No changes detected"
 fi
